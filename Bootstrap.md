adad6cabcfacb2795a20024bfe27b31908ca0bf3
btu1a213 <94585860+btula213@users.noreply.github.com>
2021-11-28 10:47:22 -0800

    Refactored Map Implementation  to abstract ID generation. Closes #23

--- Bootstrap.md
+++ Bootstrap.md
@@ -4,3 +4,7 @@
 2. Create a class DataLoader that implements CommandLineRunner
 3. After the spring initializes, the CLR's Run method is executed to load the data.
    1. Check DataLoader.java
+
+### How to generate id for Map
+   To auto-assign ID value, you can do Collections.max()+1;
+   Check Dataloader.java getNextId() method.
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -24,21 +24,18 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.print("Loading Owners");
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Jackson");
         ownerService.save(owner1);
         System.out.print(" .... ...");
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("John");
         owner2.setLastName("Lenon");
         ownerService.save(owner2);
         System.out.print(" .... ...");
 
         Owner owner3 = new Owner();
-        owner3.setId(3L);
         owner3.setFirstName("George");
         owner3.setLastName("Michael");
         ownerService.save(owner3);
@@ -48,21 +45,18 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.print("Loading Vets");
         Vet vet1 = new Vet();
-        vet1.setId(11L);
         vet1.setFirstName("Sanjay");
         vet1.setLastName("Gupta");
         vetService.save(vet1);
         System.out.print(" .... ...");
 
         Vet vet2 = new Vet();
-        vet2.setId(12L);
         vet2.setFirstName("Ray");
         vet2.setLastName("Charles");
         vetService.save(vet2);
         System.out.print(" .... ...");
 
         Vet vet3 = new Vet();
-        vet3.setId(13L);
         vet3.setFirstName("Micael");
         vet3.setLastName("Moore");
         vetService.save(vet3);
